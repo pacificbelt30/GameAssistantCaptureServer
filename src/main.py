431745c427ee228411487fe5b19449b82ff3a29a
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2024-10-24 20:35:05 +0900

    operation confirmation with Windows10

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 from typing import Tuple
 import os
 import io
-# import pyautogui
+import base64
+import pyautogui
 from fastapi import FastAPI, Response
 
 
-async def capture_screen(initial_point: Tuple[int, int]=(0, 0), width: int=600, height: int=600) -> io.BytesIO:
+def capture_screen(initial_point: Tuple[int, int]=(0, 0), width: int=600, height: int=600) -> str:
     # 特定の領域をスクリーンショットとして撮影
-    screenshot = await pyautogui.screenshot(region=(*initial_point, width, height))
+    screenshot = pyautogui.screenshot(region=(*initial_point, width, height))
     output = io.BytesIO()
-    await screenshot.save(output)
+    screenshot.save(output)
+    output.seek(0)
+    b64 = base64.b64encode(output.read())
 
-    return output
+    return b64.decode('utf-8')
 
 app = FastAPI()
 
@@ -20,8 +23,8 @@ async def main():
     return 'This is Windows Screen Capture Server'
 
 @app.get('/capture')
-async def plot_png():
-    png = await capture_screen()
+async def plot_png(init_x:int = 0, init_y:int = 0, width:int = 600, height:int = 600):
+    png = capture_screen((init_x, init_y), width, height)
+    output = {'img': 'data:image/png;base64,'+png}
     # 画像ファイルをクライアントに送信
-    png.seek(0)
-    return Response(content=png, media_type="/image/png")
+    return output
